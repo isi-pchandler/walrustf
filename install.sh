c71e455522f31ff9762c2ace78fd366d857b64bf
Ryan Goodfellow <rgoodfel@isi.edu>
2017-10-27 16:09:11 -0700

    installer fixes

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 version="0.1"
-src="v$version"
-archive="$src.tar.gz"
+src="walrustf-0.1"
+archive="v$version.tar.gz"
 
 cd /tmp
 curl -OL "https://github.com/rcgoodfellow/walrustf/archive/$archive"
 tar xzf "$archive"
 cd $src
 
-mkdir /usr/local/lib/wtf
+mkdir -p /usr/local/lib/wtf
 cp perl/Walrus.pm /usr/local/lib/wtf/
-cp python/Walrus.py /usr/local/lib/wtf/
+cp python/walrus.py /usr/local/lib/wtf/
 
 #TODO detect C and Go development runtimes and build and install
 #     if they are present
